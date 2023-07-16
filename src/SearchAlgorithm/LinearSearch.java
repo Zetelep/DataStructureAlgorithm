4dd971d3e759f86741899b5aa274aa8f05899e82
ZULFA AULIYA AKBAR <noro58.12@gmail.com>
2023-07-17 01:49:02 +0800

    bleh

--- src/SearchAlgorithm/LinearSearch.java
+++ src/SearchAlgorithm/LinearSearch.java
@@ -1,5 +1,18 @@
 package SearchAlgorithm;
 
 public class LinearSearch {
+    public static int Linear_Search(int[] array, int target) {
 
+        for (int i = 0; i <= array.length; i++) {
+            if (array[i] == target) return i;
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+       int Array[] = {1,2,3,4,5,7,8};
+       int LinearSearchtest = Linear_Search(Array,3);
+       System.out.println(LinearSearchtest); // 2
+    }
 }
