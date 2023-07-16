1cf05032e798fd0d0547d4d3caa2e6974bc24116
ZULFA AULIYA AKBAR <noro58.12@gmail.com>
2023-07-17 03:03:55 +0800

    bleh

--- src/SearchAlgorithm/BinarySearch.java
+++ src/SearchAlgorithm/BinarySearch.java
@@ -1,5 +1,32 @@
 package SearchAlgorithm;
 
 public class BinarySearch {
+    public static int Binary_Search(int[] SortedArray,int target){
+        int N = SortedArray.length;
+        int low = 0;
+        int high = N-1;
 
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if (SortedArray[mid]<target){
+                low=mid+1;
+            } else if (SortedArray[mid]>target) {
+                high = mid-1;
+            } else if (SortedArray[mid] == target) {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,6,7,8,9,12};
+        int target = 12;
+        int target2 =10;
+        int binarySearchTest = Binary_Search(arr,target);
+        int binarySearchTest2 = Binary_Search(arr,target2);
+
+        System.out.println(binarySearchTest); // 7
+        System.out.println(binarySearchTest2); // -1
+    }
 }
