e06f879eb332258587dd1b7b4bf1de81dc7b28f5
ZULFA AULIYA AKBAR <noro58.12@gmail.com>
2023-08-19 23:29:25 +0800

    typo

--- src/SortingAlgorithm/SelectionsSort.java
+++ src/SortingAlgorithm/SelectionsSort.java
@@ -23,7 +23,7 @@ public class SelectionsSort {
 
     public static void main(String[] args) {
         int[] arr = {3,6,1,8,4,5};
-        Selection_Sort(arr); // [5, 6, 7, 8, 9]
+        Selection_Sort(arr); // [1, 3, 4, 5, 6, 8]
     }
 
 }
