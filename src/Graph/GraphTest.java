5f37685bb7d84f9891ec95765645941d240cf93a
ZULFA AULIYA AKBAR <noro58.12@gmail.com>
2023-08-29 00:32:45 +0800

    added graph&Adjacency matrix

---
+++ src/Graph/GraphTest.java
@@ -0,0 +1,23 @@
+package Graph;
+
+public class GraphTest {
+    public static void main(String[] args) {
+        GraphMatrix graph = new GraphMatrix(5);
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0,1);
+        graph.addEdge(1,2);
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+        graph.addEdge(4,0);
+        graph.addEdge(4,2);
+
+        graph.print();
+
+        System.out.println(graph.checkEdge(0,1)); //checking is there an edge in A and B
+    }
+}
---
+++ src/Graph/Node.java
@@ -0,0 +1,8 @@
+package Graph;
+
+public class Node {
+    char data;
+    Node(char data){
+        this.data = data;
+    }
+}
