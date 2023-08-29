61c61cb8e796e83fef1b243b12a006c1439eabe3
ZULFA AULIYA AKBAR <noro58.12@gmail.com>
2023-08-29 23:43:00 +0800

    added Adjacency list

--- src/Graph/GraphTest.java
+++ src/Graph/GraphTest.java
@@ -2,6 +2,7 @@ package Graph;
 
 public class GraphTest {
     public static void main(String[] args) {
+        //Matrix Adjacency Graph
         GraphMatrix graph = new GraphMatrix(5);
         graph.addNode(new Node('A'));
         graph.addNode(new Node('B'));
@@ -19,5 +20,22 @@ public class GraphTest {
         graph.print();
 
         System.out.println(graph.checkEdge(0,1)); //checking is there an edge in A and B
+
+        //List Adjacency Graph
+        GraphList graph2 = new GraphList();
+        graph2.addNode(new Node('A'));
+        graph2.addNode(new Node('B'));
+        graph2.addNode(new Node('C'));
+        graph2.addNode(new Node('D'));
+        graph2.addNode(new Node('E'));
+
+        graph2.addEdge(0,1);
+        graph2.addEdge(1,2);
+        graph2.addEdge(2,3);
+        graph2.addEdge(2,4);
+        graph2.addEdge(4,0);
+        graph2.addEdge(4,2);
+        graph2.print();
+
     }
 }
